077da71b202044fbf9083529c1a658795c9d7aef
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-14 15:53:33 -0500

    Compelete Better Fixed way.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Author author = new Author("Sandi", "Metz");
-
+        System.out.println("author.fullName() = " + author.fullName());
         author.addBook("Practical Object-Oriented Design in Ruby");
         author.addBook("99 Bottles of OOP");
 
-        for (String book: author.getBooks()) {
+        for (String book: author.publishedBooks()) {
             System.out.println(book);
         }
     }
